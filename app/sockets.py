b19a09c2ecbc2d64723edd22c327b52213ff10a4
Moderator11 <p7925221@gmail.com>
2025-05-28 14:07:49 +0900

    fix: Fix currentUser handle
    - currentUser now correctly added and removed when user enter or leave
    - room deletion when no users are in the room
    - **#15 multiple identical user name handling is needed**

--- app/sockets.py
+++ app/sockets.py
@@ -1,4 +1,5 @@
-from flask_socketio import join_room
+from flask_socketio import join_room, leave_room
+from flask import request
 from . import rooms, users
 
 def register_socket_events(socketio):
@@ -12,33 +13,35 @@ def register_socket_events(socketio):
 
     @socketio.on("join_room")
     def on_join(data):
-        room_id = data.get("room_id")
+        room_id = int(data.get("room_id"))
         user = data.get("user")
         join_room(room_id)
 
-        if room_id in rooms:
-            print("id in room")
-            room = rooms[room_id]
-            if user not in room.currentUsers:
-                room.currentUsers.append(user)
+        if room_id in rooms.keys():
+            if user not in rooms[room_id].currentUsers:
+                rooms[room_id].currentUsers.append(user)
+        else:
+            print(f"Room {room_id} not found")
 
         print(f"{user} joined room {room_id}")
         socketio.emit("user_joined", {"user": user}, room=room_id)
 
     @socketio.on("leave_room")
     def on_leave(data):
-        room_id = data.get("room_id")
+        room_id = int(data.get("room_id"))
         user = data.get("user")
         leave_room(room_id)
 
-        if room_id in rooms:
-            room = rooms[room_id]
-            if user in room.currentUsers:
-                room.currentUsers.remove(user)
-                print(f"{user} removed from room {room_id}")
+        if room_id in rooms.keys():
+            if user in rooms[room_id].currentUsers:
+                rooms[room_id].currentUsers.remove(user)
+            if len(rooms[room_id].currentUsers) == 0:
+                del rooms[room_id]
+                print(f"Room {room_id} deleted due to no user")
         else:
             print(f"Room {room_id} not found")
-
+            
+        print(f"{user} left room {room_id}")
         socketio.emit("user_left", {"user": user}, room=room_id)
 
 
@@ -46,15 +49,19 @@ def register_socket_events(socketio):
     def on_message(data):
         room_id = data.get("room_id")
         message_type = data.get("message_type")
-        print(f"sent {message_type} in {room_id}")
+        print(f"sent {message_type} from {room_id}")
 
         match message_type:
             case "msg":
                 message = data.get("message")
                 socketio.emit("receive_message", {"message": message}, room=room_id)
             case "play":
-                socketio.emit("broadcast_play", {}, room=room_id)
-            case "stop":
-                socketio.emit("broadcast_stop", {}, room=room_id)
+                broadcast_time = data.get("time")
+                socketio.emit("broadcast_play", {}, room=room_id, include_self=False)
+            case "pause":
+                broadcast_time = data.get("time")
+                socketio.emit("broadcast_pause", {}, room=room_id, include_self=False)
+            case "ping":
+                socketio.emit("pong", {}, to=request.sid)
             case _:
                 print("Wrong control signal")
