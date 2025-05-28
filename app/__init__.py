c4f32d9ccced7016a24ed4e7138033e5ff7b5eac
Moderator11 <p7925221@gmail.com>
2025-05-28 15:39:33 +0900

    refact: Refactor initializer for app
    - Initializer for app & socket refactored

--- app/__init__.py
+++ app/__init__.py
@@ -4,23 +4,22 @@ from flask_socketio import SocketIO
 
 rooms = {}
 users = {}
+allowed_origins=["https://sootation.synology.me:8001", "http://localhost:5173"]
 
-#init socket
-socketio = SocketIO(cors_allowed_origins=["https://sootation.synology.me:8001", "http://localhost:5173"])
+socketio = SocketIO(cors_allowed_origins=allowed_origins)
 
 def create_app(config_object=None):
-    """Application factory"""
-
     app = Flask(__name__)
     if config_object:
         app.config.from_object(config_object)
 
-    CORS(app, origins=["https://sootation.synology.me:8001", "http://localhost:5173"])
+    CORS(app, origins=allowed_origins)
 
     from . import routes
     app.register_blueprint(routes.api_bp)
 
-    #bind socket to app
     socketio.init_app(app)
+    from .sockets import register_socket_events
+    register_socket_events(socketio)
 
     return app
